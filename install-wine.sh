b3a92356f678ce288e7679c16475a3c23b027a66
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-04-06 17:07:42 +0100

    Update install-wine.sh

--- install-wine.sh
+++ install-wine.sh
@@ -2,12 +2,18 @@
 
 sudo dpkg --add-architecture i386
 
-curl -fsSL  https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add -
+curl -fsSL  https://dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
 
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 
 sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
 sudo apt-get update
+
+wget -O Release2.key https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key
+sudo apt-key add Release2.key
+
+sudo apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./'
 
 sudo apt-get install -y --install-recommends winehq-stable
 sudo apt-get install -y winetricks
