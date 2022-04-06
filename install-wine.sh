ae499e098028ec98765286692a85ce90f55124a5
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2022-04-06 17:13:20 +0100

    Update install-wine.sh

--- install-wine.sh
+++ install-wine.sh
@@ -19,4 +19,8 @@ sudo apt-get install -y --install-recommends winehq-stable
 sudo apt-get install -y winetricks
 sudo apt-get install -y winbind
 sudo apt-get install -y wine32
+sudo apt-get install -y libfaudio0:i386
+sudo apt-get install -y libglib2.0-0:i386
+sudo apt-get install -y libgstreamer1.0-0:i386
+sudo apt-get install -y libgstreamer-plugins-base1.0-0:i386
 sudo apt-get install -y libwine:i386
